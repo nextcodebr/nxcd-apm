8803c8e0b9e0a1c7d6a634b9c6b098519f00004d
cmuramoto <cleber.muramoto@gmail.com>
2022-02-15 02:54:42 -0300

    feat: abstract BufferingSink

--- src/sink/api/index.ts
+++ src/sink/api/index.ts
@@ -2,6 +2,32 @@ export interface ISink<T> {
   accept: (obj: T) => void
 }
 
+export abstract class BufferingSink<T> implements ISink<T> {
+  protected buffer: T[]
+
+  constructor () {
+    this.buffer = []
+  }
+
+  accept (obj: T) {
+    this.buffer.push(obj)
+  }
+
+  acceptAll (vals: T[]) {
+    this.buffer = this.buffer.concat(vals)
+  }
+
+  async flush (): Promise<void> {
+    const len = this.buffer.length
+    if (len > 0) {
+      const copy = this.buffer.splice(0, len)
+      await this.ingest(copy)
+    }
+  }
+
+  abstract ingest (slice: T[]): Promise<void>
+}
+
 export const BlackHole = <T> (): ISink<T> => {
   return {
     accept: (obj: T): void => {
